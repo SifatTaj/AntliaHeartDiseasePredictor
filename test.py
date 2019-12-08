e49835dd9b89673d7e78bb2a99f90872953a9295
SifatTaj <sifat.taj@gmail.com>
2019-12-09 01:26:12 +0600

    Input tested

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ Window.set_title('Antlia Heart Disease Predictor')
 
 
 class MyLayout(Widget):
+
     name = ObjectProperty(None)
     age = ObjectProperty(None)
     sex = ObjectProperty(None)
@@ -28,7 +29,59 @@ class MyLayout(Widget):
     thal = ObjectProperty(None)
 
     def on_pressed(self):
-        print(self.sex.state)
+
+        patient = ""
+
+        patient = patient + self.age.text + ","
+        patient = patient + ("1" if self.sex.state == "down" else "0") + ","
+
+        if self.cp1.state == "down":
+            patient = patient + "3" + ","
+
+        elif self.cp2.state == "down":
+            patient = patient + "2" + ","
+
+        else:
+            patient = patient + "1" + ","
+
+        patient = patient + self.bp.text + ","
+        patient = patient + self.chol.text + ","
+        patient = patient + ("1" if int(self.fbs.text) > 120 else "0") + ","
+
+        if self.restecg.text == "normal":
+            patient = patient + "0" + ","
+
+        elif self.restecg.text == "ST-T":
+            patient = patient + "1" + ","
+
+        else:
+            patient = patient + "2" + ","
+
+        patient = patient + self.thalach.text + ","
+        patient = patient + ("1" if self.exang.state == "down" else "0") + ","
+        patient = patient + self.oldpeak.text + ","
+
+        if self.slope.text == "unsloping":
+            patient = patient + "1" + ","
+
+        elif self.slope.text == "flat":
+            patient = patient + "2" + ","
+
+        else:
+            patient = patient + "3" + ","
+
+        patient = patient + self.ca.text + ","
+
+        if self.thal.text == "normal":
+            patient = patient + "1" + ","
+
+        elif self.thal.text == "fixed":
+            patient = patient + "2" + ","
+
+        else:
+            patient = patient + "3" + ","
+
+        print(patient)
 
 
 class MyApp(App):
