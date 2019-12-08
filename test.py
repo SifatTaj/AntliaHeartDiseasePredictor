3b786ad0d13af4e8bc478183a68724cf8b3c5c9f
SifatTaj <sifat.taj@gmail.com>
2019-12-09 00:51:07 +0600

    UI Finalized

--- test.py
+++ test.py
@@ -4,20 +4,37 @@ from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.lang import Builder
 
 Window.clearcolor = (.3, .3, .3, 1)
+Window.set_title('Antlia Heart Disease Predictor')
 
 
-# class FloatLayout(Widget):
-#     name = ObjectProperty(None)
-#
-#     def on_pressed(self):
-#         print(self.name.text)
+class MyLayout(Widget):
+    name = ObjectProperty(None)
+    age = ObjectProperty(None)
+    sex = ObjectProperty(None)
+    cp1 = ObjectProperty(None)
+    cp2 = ObjectProperty(None)
+    bp = ObjectProperty(None)
+    chol = ObjectProperty(None)
+    fbs = ObjectProperty(None)
+    restecg = ObjectProperty(None)
+    thalach = ObjectProperty(None)
+    exang = ObjectProperty(None)
+    oldpeak = ObjectProperty(None)
+    slope = ObjectProperty(None)
+    ca = ObjectProperty(None)
+    thal = ObjectProperty(None)
+
+    def on_pressed(self):
+        print(self.sex.state)
 
 
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        self.title = "Antlia Heart Disease Predictor"
+        return MyLayout()
 
 
 if __name__ == "__main__":
